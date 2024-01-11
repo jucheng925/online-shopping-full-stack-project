156a55af7f50414314203966e3d326ea4056f655
Julie Cheng <jucheng925@gmail.com>
2024-01-11 12:53:48 -0500

    added user table with seeded data

--- server/app.py
+++ server/app.py
@@ -1,4 +1,5 @@
-from config import app
+from config import app, db, Api
+from models.user import User
 
 if __name__ == "__main__":
   app.run(port=5555, debug=True)
--- server/seed.py
+++ server/seed.py
@@ -1,6 +1,38 @@
 from config import app, db
 
+from faker import Faker
+
+from models.user import User
+
 if __name__ == "__main__":
-  with app.app_context():
-    pass
-    # remove pass and write your seed data
+	with app.app_context():
+		
+		print("Deleting all records ...")
+		User.query.delete()
+
+		fake = Faker()
+
+		print("Creating users...")
+
+		users = []
+		usernames = []
+
+		for i in range(10):
+			username = fake.first_name()
+			while username in usernames:
+				username = fake.first_name()
+			usernames.append(username)
+			
+			user = User(
+				username=username,
+				isAdmin = fake.boolean()
+			)
+
+			user.password_hash = user.username + 'password'
+
+			users.append(user)
+			
+		db.session.add_all(users)
+		
+		db.session.commit()
+
