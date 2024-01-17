06b49cc17c427ce66eb66d615e141a56ff07922a
Julie Cheng <jucheng925@gmail.com>
2024-01-17 10:59:07 -0500

    sign up connecting front and backend

--- client/src/components/Signup.jsx
+++ client/src/components/Signup.jsx
@@ -6,20 +6,24 @@ const Signup = () => {
     const formSchema = yup.object().shape({
         username: yup.string().required("Must enter a username").max(10),
         password: yup.string().required("Must enter a password").max(15),
-        isAdmin: yup.boolean(),
+        isAdmin: yup.string().oneOf(["yes", "no"]).required("Please select one")
     });
 
     const formik = useFormik({
         initialValues: {
             username: "",
             password: "",
-            isAdmin: false,
+            isAdmin: '',
         },
         validationSchema: formSchema,
         onSubmit: submitform
     })
 
     function submitform(values) {
+        values.isAdmin == 'yes' ?
+            values.isAdmin = true :
+            values.isAdmin = false;
+        
         fetch("/api/users", {
             method: "POST",
             headers: {
@@ -40,8 +44,11 @@ const Signup = () => {
         <label htmlFor="password">Password</label>
         <input type="text" name="password" id="password" value={formik.values.password} onChange={formik.handleChange}/>
         <br/>
-        <label htmlFor="isAdmin">Admin?</label>
-        <input type="radio" id="isAdmin" name="isAdmin" value ={formik.values.isAdmin} onChange={formik.handleChange}/>
+        <h4>Is Admin?</h4>
+        <label htmlFor="yes">YES</label>
+        <input type="radio" name="isAdmin" value="yes" id="yes" checked={formik.values.isAdmin == 'yes'} onChange={formik.handleChange}/>
+        <label htmlFor="no">NO</label>
+        <input type="radio" name="isAdmin" value="no" id="no" checked={formik.values.isAdmin == 'no'} onChange={formik.handleChange}/>
         <button type="submit">Submit</button>
     </form>
   )
--- server/app.py
+++ server/app.py
@@ -6,9 +6,22 @@ from config import app, db, api
 from models.models import User
 
 class Signup(Resource):
-  pass
+  def post(self):
+    data = request.get_json()
+    
+    username = data.get('username')
+    isAdmin = data.get('isAdmin')
 
-api.add_resource(Signup, '/api/users')
+    user = User(username=username, isAdmin=isAdmin)
+    user.password_hash = data.get('password')
+
+    db.session.add(user)
+    db.session.commit()
+
+    return user.to_dict(), 201
+
+
+api.add_resource(Signup, '/users')
 
 if __name__ == "__main__":
   app.run(port=5555, debug=True)
